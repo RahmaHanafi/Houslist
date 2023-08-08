7e5d7dd55ea33bed0d5fa7a45540430f47a8dcd8
RahmaHanafi <rahmahanafi809@gmail.com>
2023-08-08 12:27:18 +0300

    handle responsive2

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -5,7 +5,7 @@ export default function Title({
   textPosition,
 }) {
   return (
-    <div className="relative mx-auto md:mx-0">
+    <div className="relative mx-auto md:mx-0 max-sm:w-10/12">
       <p className="bg-font text-5xl text-center lg:text-9xl md:text-start text-gray-200 ">
         {bgTitle}
       </p>
